05b4174dfe4cfeeada9501c076e10d0bb42625e2
rbsantos-code <richardbriansantos@Richards-MacBook-Pro.local>
2021-05-23 16:42:32 -0700

    create user-model

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { getAllPizza, getPizzaById, createPizza, updatePizza, deletePizza } = require('../../controllers/pizza-controller');
+
+// Set up Get all and Post at /api/pizzas
+
+router
+    .route('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+
+// Set up Get one, and Delete at /api/pizzas/:id
+
+router
+    .route('/:id')
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
+
+
+module.exports = router;
